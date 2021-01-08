c52abff5bd967bf5c5e7f72eb1e448ebf5b9f626
jeremylai7 <1049024592@qq.com>
2021-01-08 10:10:04 +0800

    添加序列化

---
+++ basis/src/main/java/SerializationTest.java
@@ -0,0 +1,44 @@
+import org.junit.Test;
+
+import java.io.*;
+
+/**
+ * @Author: laizc
+ * @Date: Created in  2021-01-08
+ * @desc: 序列化测试
+ */
+public class SerializationTest {
+
+	@Test
+	public void test2() {
+		User user = new User();
+		user.setName("aa");
+		user.setPassword("bb");
+		try {
+			FileOutputStream fileOutputStream = new FileOutputStream("/aa.txt");
+			ObjectOutputStream outputStream = new ObjectOutputStream(fileOutputStream);
+			outputStream.writeObject(user);
+			outputStream.close();
+			fileOutputStream.close();
+		}catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void test3() {
+		User user = null;
+		try {
+			FileInputStream fileInputStream = new FileInputStream("/aa.txt");
+			ObjectInputStream inputStream = new ObjectInputStream(fileInputStream);
+			user = (User) inputStream.readObject();
+			inputStream.close();
+			fileInputStream.close();
+		}catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		System.out.println(user.getName() + " " + user.getPassword());
+	}
+}
---
+++ basis/src/main/java/User.java
@@ -0,0 +1,27 @@
+/**
+ * @Author: laizc
+ * @Date: Created in  2021-01-08
+ * @desc:
+ */
+public class User {
+
+	private String name;
+
+	private String password;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
