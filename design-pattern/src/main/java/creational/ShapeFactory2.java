81d900aef3dea34ed537789fb412dd1586fd6f3f
jeremylai7 <1049024592@qq.com>
2021-01-25 09:46:38 +0800

    根据类的class获取不同的属性

---
+++ design-pattern/src/main/java/creational/ShapeFactory2.java
@@ -0,0 +1,36 @@
+package creational;
+
+/**
+ * @Author: laizc
+ * @Date: Created in  2021-01-25
+ * @desc: 创建工厂方法，根据不同的类获取不同的实例
+ */
+public class ShapeFactory2 extends AbstractShapeFactory2{
+
+	@Override
+	public <T extends Shape> T getShape(Class<T> tClass) {
+		Shape shape;
+		try {
+			shape =  (T) Class.forName(tClass.getName()).newInstance();
+			return (T) shape;
+		} catch (InstantiationException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+
+	public static void main(String[] args) {
+		ShapeFactory2 factory = new ShapeFactory2();
+		Shape circleShape = factory.getShape(Circle.class);
+		circleShape.draw();
+
+		Shape rectangleShape = factory.getShape(Rectangle.class);
+		rectangleShape.draw();
+	}
+
+}
