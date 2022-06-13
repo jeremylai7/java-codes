d6e5d4b46e33bb8940138ce8cfb5ae174ad7e729
jeremylai7 <1049024592@qq.com>
2022-06-14 00:30:45 +0800

    测试 cpu 密集型

---
+++ concurrent/src/main/java/threadpool/setSize/CpuTypeTest.java
@@ -0,0 +1,79 @@
+package threadpool.setSize;
+
+import java.util.List;
+
+/**
+ * @author: laizc
+ * @date: created in 2022/6/13
+ * @desc:
+ **/
+public class CpuTypeTest implements Runnable{
+
+    // 整体执行时间，包括在队列中等待的时间
+    List<Long> wholeTimeList;
+
+    // 真正执行的时间
+    List<Long> runTimeList;
+
+    private long initStarTime = 0;
+
+    public CpuTypeTest(List<Long> runTimeList,List<Long> wholeTimeList) {
+        initStarTime = System.currentTimeMillis();
+        this.wholeTimeList = wholeTimeList;
+        this.runTimeList = runTimeList;
+    }
+
+
+    @Override
+    public void run() {
+        long start = System.currentTimeMillis();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        countPrimes(1,1000000);
+        long end = System.currentTimeMillis();
+        long wholeTime = end - initStarTime;
+        long runTime = end - start;
+        wholeTimeList.add(wholeTime);
+        runTimeList.add(runTime);
+        //System.out.println("单个线程花费时间：" + (end - start));
+
+    }
+
+    /**
+     * 判断素数
+     * @param number
+     * @return
+     */
+    public boolean isPrime(final int number) {
+        if (number <= 1) {
+            return false;
+        }
+        for (int i = 2; i < Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * 计算素数
+     * @param lower
+     * @param upper
+     * @return
+     */
+    public int countPrimes(final int lower,final int upper) {
+        int total = 0;
+        for (int i = 0; i <= upper; i++) {
+            if (isPrime(i)) {
+                total++;
+            }
+        }
+        return total;
+    }
+
+
+}
---
+++ concurrent/src/main/java/threadpool/setSize/SetSizeTest.java
@@ -0,0 +1,58 @@
+package threadpool.setSize;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+import java.util.concurrent.*;
+
+/**
+ * @author: laizc
+ * @date: created in 2022/6/13
+ * @desc: 测试核心线程数设置
+ **/
+public class SetSizeTest {
+
+    private static int coreSize = 3;
+
+    private static ThreadPoolExecutor threadPool = new ThreadPoolExecutor(coreSize,coreSize,10, TimeUnit.SECONDS,new ArrayBlockingQueue<>(1000),new ThreadPoolExecutor.DiscardOldestPolicy());
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException, TimeoutException {
+        int cores = Runtime.getRuntime().availableProcessors();
+        System.out.println("CPU核数：" + cores + " ");
+        int requestNum = 10;
+        List<Future<?>> futureList = new ArrayList<>();
+
+        Vector<Long> wholeTimeList = new Vector<>();
+        Vector<Long> runTimeList = new Vector<>();
+
+        for (int i = 0; i < requestNum; i++) {
+            Future<?> future = threadPool.submit(new CpuTypeTest(runTimeList,wholeTimeList));
+            futureList.add(future);
+        }
+
+        for (Future<?> future : futureList) {
+            // 获取线程执行结果
+            future.get(requestNum,TimeUnit.SECONDS);
+        }
+
+        long wholeTime = 0;
+        // 时间累加
+        for (int i = 0; i < wholeTimeList.size(); i++) {
+            wholeTime = wholeTimeList.get(i) + wholeTime;
+        }
+
+        long runTime = 0;
+        for (int i = 0; i < runTimeList.size(); i++) {
+            runTime = runTimeList.get(i) + runTime;
+        }
+
+        System.out.println("平均每个线程整体花费时间:" + wholeTime/wholeTimeList.size());
+        System.out.println("平均每个线程执行花费时间:" + runTime/runTimeList.size());
+
+
+
+
+
+    }
+
+}
