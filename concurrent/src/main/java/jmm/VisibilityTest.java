0576b916a58dad3cbf03c799c71bd0b4aeb91715
jeremylai7 <1049024592@qq.com>
2022-09-02 16:09:48 +0800

    可见性测试

--- concurrent/src/main/java/jmm/VisibilityTest.java
+++ concurrent/src/main/java/jmm/VisibilityTest.java
@@ -30,8 +30,51 @@ public class VisibilityTest {
         // 等待两个线程执行结束
         th1.join();
         th2.join();
+        // 结果小于 20000
         System.out.println(count);
     }
 
+    private static volatile boolean flag = true;
+    //private static boolean flag = true;
+
+    @Test
+    public void test2() {
+        new Thread1().start();
+        try {
+            // 暂停一秒，保证线程1 启动并运行
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        new Thread2().start();
+
+    }
+
+    class Thread1 extends Thread {
+        @Override
+        public void run() {
+            while (flag) {
+                // flag 为 true，一直读取flag字段，flag 为 false 时跳出来。
+                //System.out.println("一直在读------" + flag);
+            }
+            System.out.println("thread - 1 跳出来了");
+        }
+    }
+
+    class Thread2 extends Thread {
+
+        @Override
+        public void run() {
+            /*try {
+                // 休眠两秒
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }*/
+            System.out.println("thread-2 run");
+            flag = false;
+            System.out.println("flag 改成 false");
+        }
+    }
 
 }
