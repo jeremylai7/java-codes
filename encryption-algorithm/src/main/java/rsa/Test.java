6aaa8bdce89d509ee54abd5bc73319cc2e064eef
jeremylai7 <1049024592@qq.com>
2023-05-13 16:34:34 +0800

    修改加密方式

--- encryption-algorithm/src/main/java/rsa/Test.java
+++ encryption-algorithm/src/main/java/rsa/Test.java
@@ -7,19 +7,25 @@ package rsa;
  */
 public class Test {
 
-    private static String publicKey = "MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAK9yIG711y8nxP4Cgdov0746tnTKzoB6pV/FwwzI/xs+pj3XmQkrJdVLLrUQYqUsoQ7W3/00LKzbaZ4lT9HxqkECAwEAAQ==";
-
-    private static String privateKey = "MIIBVQIBADANBgkqhkiG9w0BAQEFAASCAT8wggE7AgEAAkEAr3IgbvXXLyfE/gKB2i/Tvjq2dMrOgHqlX8XDDMj/Gz6mPdeZCSsl1UsutRBipSyhDtbf/TQsrNtpniVP0fGqQQIDAQABAkByclFD8Kx5P0kapYoNSUun+ObSkwVX/ClqqzfMXEVFgGbx0SduGOZkViahFxJjr2Lj3pcoIA3pMfQPoh8HY1itAiEA8NEO6KCXtw1S0XuLXgTDFeEHAFMRCetRLcREAvpNSxMCIQC6gfCFWJp+74+HklBa9BSLUjYPwdroBEFCr6Hq33jr2wIhAL+7g2Y9rm2FAyJcffruSbDayCs+97rxC2In+IpyZSXXAiA+2md6KL/4IzeVkqNt42/A+/WoHcZuBnEPziydAK65nQIhAJQZLupv83exZ9G3FmeHzQwHTIp6QZy/XiYKMPfMNnZo";
+    private static String publicKey = "MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAI3NwBNv8VcOJTnYKLi5saGq4SxfkoClq9LyWWXoK94sYNWW5+GgDxKRmc01ZebzDplFQikgbp5rB/x2HrkDMvkCAwEAAQ==";
+    private static String privateKey = "MIIBVQIBADANBgkqhkiG9w0BAQEFAASCAT8wggE7AgEAAkEAjc3AE2/xVw4lOdgouLmxoarhLF+SgKWr0vJZZegr3ixg1Zbn4aAPEpGZzTVl5vMOmUVCKSBunmsH/HYeuQMy+QIDAQABAkAy6HLUh5O1FTb4FUpJRyEmURqVu5n05YVSLf86PqfBEtAp57jZuAKRyKz836br5dsFeSFu7uzb6nAWhE6jRd6hAiEA9r862fCWssNSxUJzxz63BFfc0p35hZmrlrbkvo3AYQsCIQCTHxC6+MYzuhoGAqd3A1Gjl8x2a2Bn5komJC6F84DGiwIhAL+CuPjCpRRmSW3VH+t7H619TGUkO7CYDvfRSEUl3DT1AiB4BOfJIBnRKQkBzjh/xp1bMAMEuCQ1npDu/KVXcYakcQIhAKW7QDuOzZfB7ic9GErfLeuDrQGF3tNJyj5pALpmlqeR";
 
     public static void main(String[] args) {
-        String source = "laizc3333";
+        String source = "123456哈哈哈";
         //私钥加密，公钥解密
-        /*String privateKeyEncrypt = RsaUtil.privateKeyEncrypt(privateKey,source);
-        String publicKeyDecrypt = RsaUtil.publicKeyDecrypt(publicKey,privateKeyEncrypt);*/
+        String privateKeyEncrypt = RsaUtil.privateKeyEncrypt(privateKey,source);
+        System.out.println("公钥加密："+ privateKeyEncrypt);
+        String publicKeyDecrypt = RsaUtil.publicKeyDecrypt(publicKey,privateKeyEncrypt);
+        System.out.println("公钥解密："+ publicKeyDecrypt);
+
         //公钥加密私钥解密
         String publicKeyEncrypt = RsaUtil.publicKeyEncrypt(publicKey,source);
+        System.out.println("私钥加密：" + publicKeyEncrypt);
         String privateKeyDecrypt =  RsaUtil.privateKeyDecrypt(privateKey,publicKeyEncrypt);
-        System.out.println(privateKeyDecrypt);
+        System.out.println("私钥解密：" + privateKeyDecrypt);
+
+        // 私钥加密  公钥解密
+
 
 
 
